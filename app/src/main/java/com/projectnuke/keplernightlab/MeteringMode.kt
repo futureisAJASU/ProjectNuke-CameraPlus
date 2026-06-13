5f61ca17c12b28c44b6d241061fdd6cb26fdfb68
AJASU <junyoung090928@gmail.com>
2026-06-13 21:17:56 +0900

    Expand metering modes and shared state

--- app/src/main/java/com/projectnuke/keplernightlab/MeteringMode.kt
+++ app/src/main/java/com/projectnuke/keplernightlab/MeteringMode.kt
@@ -1,16 +1,31 @@
 package com.projectnuke.keplernightlab
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
 enum class MeteringMode(
     val label: String,
     val shortLabel: String
 ) {
-    MATRIX("Matrix", "MAT"),
+    AVERAGE("Average", "AVG"),
+    CENTER_WEIGHTED("Center-weighted", "CW"),
     CENTER("Center", "CTR"),
     SPOT("Spot", "SPT");
 
     fun next(): MeteringMode = when (this) {
-        MATRIX -> CENTER
+        AVERAGE -> CENTER_WEIGHTED
+        CENTER_WEIGHTED -> CENTER
         CENTER -> SPOT
-        SPOT -> MATRIX
+        SPOT -> AVERAGE
+    }
+}
+
+object MeteringModeState {
+    var mode by mutableStateOf(MeteringMode.CENTER)
+        private set
+
+    fun cycle() {
+        mode = mode.next()
     }
 }
