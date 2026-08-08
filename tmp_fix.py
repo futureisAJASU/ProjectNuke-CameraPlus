026b9b665188f5e805acd6285c7cd09d23a3f70b
futureisAJASU <junyoung090928@gmail.com>
2026-08-08 21:06:08 +0900

    chore: remove temporary files

--- tmp_fix.py
+++
@@ -1,8 +0,0 @@
-﻿lines = open("C:/Users/junyo/AndroidStudioProjects/KeplerNightLab/app/src/main/java/com/projectnuke/keplernightlab/RawFusionCapture.kt", encoding="utf-8").read().split(chr(10))
-for i in range(len(lines)):
-    stripped = lines[i].strip()
-    if stripped in ("partial = false,", "partial = true,"):
-        if i + 1 < len(lines) and lines[i + 1].strip() == ")":
-            lines[i] = lines[i].rstrip(",")
-open("C:/Users/junyo/AndroidStudioProjects/KeplerNightLab/app/src/main/java/com/projectnuke/keplernightlab/RawFusionCapture.kt", "w", encoding="utf-8").write(chr(10).join(lines))
-print("fixed trailing commas")
